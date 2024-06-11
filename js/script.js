bec9aed5acbf1caf0e20ae1cc18f2d03c29a047d
Anurag7102 <anuragsehgal002@gmail.com>
2024-06-11 11:16:22 +0530

    This is a snake game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let inputDir = {x: 0, y:0};
-const foodSound = new Audio ('../snake-game/music/food.mp3');
-const gameOverSound = new Audio ('../snake-game/music/gameover.mp3');
-const moveSound = new Audio ('../snake-game/music/move.mp3');
-const music = new Audio ('../snake-game/music/music.mp3');
+const foodSound = new Audio ('/snake-game/music/food.mp3');
+const gameOverSound = new Audio ('/snake-game/music/gameover.mp3');
+const moveSound = new Audio ('/snake-game/music/move.mp3');
+const music = new Audio ('/snake-game/music/music.mp3');
 let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
